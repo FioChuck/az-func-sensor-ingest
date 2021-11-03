1b99a4bd21fcf58a1b92e58bace99ba1d8297fde
Chas Fiorenza <chasfiorenza@gmail.com>
2021-11-03 13:19:40 -0400

    move proxies

--- httpingest/__init__.py
+++ httpingest/__init__.py
@@ -6,6 +6,7 @@ import azure.functions as func
 def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    var1 = func.HttpRequest.get_body
 # test commit
     try:
         req_body = req.get_json()
