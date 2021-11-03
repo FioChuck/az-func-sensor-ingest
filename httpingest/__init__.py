0aa21fe20b2ae7525fd66b0bf8f486042f2dad0f
Chas Fiorenza <chasfiorenza@gmail.com>
2021-11-03 12:51:54 -0400

    first commit

---
+++ httpingest/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import json
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        logging.info('Error reading input json')
+        pass
+
+    input_dict = req_body
+
+    output_json = json.dumps(input_dict)
+    outputblob.set(output_json)
+
+    return func.HttpResponse(body="success")
