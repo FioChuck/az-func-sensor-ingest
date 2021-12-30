24c14e6a873c69db4f2e905711a8835d201c9c35
Chas Fiorenza <chasfiorenza@gmail.com>
2021-12-30 11:53:07 -0500

    update git action profile

--- httpingest/__init__.py
+++ httpingest/__init__.py
@@ -1,13 +1,17 @@
 import logging
 import json
+import datetime as dt
 import azure.functions as func
-import ast
+import pandas as pd
 
 def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     logging.info('http body: ' + str(req.get_body()))
 
-    var1 = '{"' + str(req.get_body())\
+    ## convert input string to output string
+    ## intermedieate dictionary created
+
+    input_str = '{"' + str(req.get_body())\
     .replace("b","")\
     .replace('"', '')\
     .replace("'", '')\
@@ -16,12 +20,24 @@ def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpRespons
     .replace("&", '","')\
     .replace("PASSKEY:", 'PASSKEY":"') + '"}'
 
-    convertedDict = json.loads(var1)
+    logging.info('output string: ' + input_str)
 
-    convertedString = json.dumps(convertedDict)
-    logging.info('json string: ' + convertedString)
+    input_json = json.loads(input_str)
 
-    outputblob.set(convertedString)
+    df = pd.json_normalize(input_json) # convert input json to pandas dataframe
+    df = df.reset_index(drop = True) # remove index column
+    df["ingest_time"] = dt.datetime.now() # add datetime
+
+    output_str = df.to_csv(index=False) # convert pandas dataframe to string
+    ## output string saved to outputblob binding (see function.json)
+
+    logging.info('output string: ' + output_str)
+
+
+    ## save output string to adls 
+
+    outputblob.set(output_str)
+
     logging.info('job complete')
 
     return "success"
