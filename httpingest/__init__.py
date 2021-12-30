56fbb8155f1c628135575197726aca2e63c042d7
Chas Fiorenza <chasfiorenza@gmail.com>
2021-12-30 15:06:20 -0500

    cosmos db bindings

--- httpingest/__init__.py
+++ httpingest/__init__.py
@@ -4,7 +4,7 @@ import datetime as dt
 import azure.functions as func
 import pandas as pd
 
-def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpResponse:
+def main(req: func.HttpRequest, doc: func.Out[bytes]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     logging.info('http body: ' + str(req.get_body()))
 
@@ -24,6 +24,20 @@ def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpRespons
 
     input_json = json.loads(input_str)
 
+## Save to Cosmos DB
+    newdocs = func.DocumentList()
+    newproduct_dict = {
+        "id": "stuff1",
+        "name": "stuff2"
+    }
+    
+    logging.info('output dict: ' + str(newproduct_dict))
+
+    newdocs.append(func.Document.from_dict(newproduct_dict))
+    doc.set(newdocs)
+
+   ## Save to Cosmos DB END 
+
     df = pd.json_normalize(input_json) # convert input json to pandas dataframe
     df = df.reset_index(drop = True) # remove index column
     df["ingest_time"] = dt.datetime.now() # add datetime
@@ -35,8 +49,7 @@ def main(req: func.HttpRequest, outputblob: func.Out[bytes]) -> func.HttpRespons
 
 
     ## save output string to adls 
-
-    outputblob.set(output_str)
+    # outputblob.set(output_str)
 
     logging.info('job complete')
 
