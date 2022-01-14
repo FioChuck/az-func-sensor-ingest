af6cd1511bc7cb496bec24fa48090b49dddded48
Chas Fiorenza <chasfiorenza@gmail.com>
2022-01-14 12:48:17 -0500

    write openweathermap to cosmos

--- weatheringest/__init__.py
+++ weatheringest/__init__.py
@@ -2,27 +2,33 @@ import logging
 import requests
 import json
 import os
-
 import azure.functions as func
 
 
-def main(mytimer: func.TimerRequest) -> None:
+def main(mytimer: func.TimerRequest, doc: func.Out[func.Document]) -> None:
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
-    
 
-url = 'http://api.openweathermap.org/data/2.5/weather?id=' + os.environ['weatherApiKey']
+    url = 'http://api.openweathermap.org/data/2.5/weather?id=' + os.environ['weatherApiKey']
 
-payload = ""
-headers = {}
+    payload = ""
+    headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
-dict1 = response.json()
+    dict1 = response.json()
+    del dict1['id']
 
-convertedString = json.dumps(response.json())
-logging.info('json string: ' + convertedString)
+    convertedString = json.dumps(response.json())
+    logging.info('json string: ' + convertedString)
 
 
+    newdocs = func.DocumentList() 
+
+    newdocs.append(func.Document.from_dict(dict1))
+
+    print(newdocs.data)
+
+    doc.set(newdocs)
