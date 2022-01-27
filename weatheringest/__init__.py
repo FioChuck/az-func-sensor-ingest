cf3921bbb1579862d0fec2562e459295d77d38c1
Chas Fiorenza <chasfiorenza@gmail.com>
2022-01-27 11:13:13 -0500

    Add city to document

--- weatheringest/__init__.py
+++ weatheringest/__init__.py
@@ -11,22 +11,42 @@ def main(mytimer: func.TimerRequest, doc: func.Out[func.Document]) -> None:
         logging.info('The timer is past due!')
 
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?id=' + os.environ['weatherApiKey']
+    url1 = 'http://api.openweathermap.org/data/2.5/weather?id=4180439&appid=' + os.environ['weatherApiKey'] # atlanta
+    url2 = 'http://api.openweathermap.org/data/2.5/weather?id=4221552&appid=' + os.environ['weatherApiKey'] # savannah
 
     payload = ""
     headers = {}
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response1 = requests.request("GET", url1, headers=headers, data=payload)
+    response2 = requests.request("GET", url2, headers=headers, data=payload)
 
-    dict1 = response.json()
-    del dict1['id']
+## atlanta ##
+    dict1 = response1.json()
+    del dict1['id'] # remove id - interferes with cosmos db id
+    del dict1['name'] # remove name - replace with city
 
-    convertedString = json.dumps(response.json())
-    logging.info('json string: ' + convertedString)
+    dict1['state'] = 'Georgia'
+    dict1['city'] = 'Atlanta'
+####
 
+## savannah ##
+    dict2 = response2.json()
+    del dict2['id'] # remove id - interferes with cosmos db id
+    del dict2['name'] # remove name - replace with city
+
+    dict2['state'] = 'Georgia'
+    dict2['city'] = 'Savannah'
+####
+
+## for logging only
+    convertedString1 = json.dumps(response1.json())
+    logging.info('json string1: ' + convertedString1)
+
+    convertedString2 = json.dumps(response2.json())
+    logging.info('json string2: ' + convertedString2)
+####
 
     newdocs = func.DocumentList() 
-
     newdocs.append(func.Document.from_dict(dict1))
 
     print(newdocs.data)
