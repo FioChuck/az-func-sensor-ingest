05a7a26ebd0c2fd4c36d1fc19e942e24f29ff15b
Chas Fiorenza <chasfiorenza@gmail.com>
2022-02-22 12:10:57 -0500

    doc structure change

--- .github/workflows/EcoWittPrep.py
+++ .github/workflows/EcoWittPrep.py
@@ -1,52 +1,10 @@
 
 import json
+import pytz
+from datetime import datetime
 
-def str_to_dict(str1):
-    str1 = '{"' + str1\
-    .replace("b","")\
-    .replace('"', '')\
-    .replace("'", '')\
-    .replace(",", '","')\
-    .replace("=", '":"')\
-    .replace("&", '","')\
-    .replace("PASSKEY:", 'PASSKEY":"') + '"}'
 
-    dict1 = json.loads(str1)
-
-    return dict1
-
-def str_map_emb(input_str,timestr):
-
-    convertedDict = str_to_dict(input_str)
-
-    sensorDict = {
-        'soilmoisture1' : float(convertedDict['soilmoisture1']),
-        'soilatt1' : float(convertedDict['soilatt1']),
-        'soilmoisture2' : float(convertedDict['soilmoisture2']),
-        'soilatt2' : float(convertedDict['soilatt2']),
-        'soilmoisture3' : float(convertedDict['soilmoisture3']),
-        'soilatt3' : float(convertedDict['soilatt3'])
-    }
-
-    outputDict = {
-        'doctype' : 'station',
-        'stationtype' : str(convertedDict['stationtype']),
-        'eventtime' : str(convertedDict['dateutc']),
-        'processingtime' : timestr,
-        'tempinf' : float(convertedDict['tempinf']),
-        'humidityin' : float(convertedDict['humidityin']),
-        'aromrelin' : float(convertedDict['aromrelin']),
-        'aromasin' : float(convertedDict['aromasin']),
-        'freq' : str(convertedDict['freq']),
-        'model' : str(convertedDict['model']),
-        'soilsensors' : sensorDict
-    }
-   
-    return outputDict
-
-def str_map_ref(input_str,timestr):
-
-    convertedDict = str_to_dict(input_str)
+def str_map(str1):
 
     str1 = '{"' + str1\
     .replace("b","")\
@@ -59,44 +17,63 @@ def str_map_ref(input_str,timestr):
 
     convertedDict = json.loads(str1)
 
-    sensorDict1 = {
-        'doctype' : 'sensor',
-        'sensorid' : '1',
-        'eventtime' : str(convertedDict['dateutc']),
-        'processingtime' : timestr,
-        'soilmoisture' : float(convertedDict['soilmoisture1']),
-        'soilatt' : float(convertedDict['soilatt1']),
+    sensorDict = {
+        'soilmoisture1' : float(convertedDict['soilmoisture1']),
+        'soilatt1' : float(convertedDict['soilatt1']),
+        'soilmoisture2' : float(convertedDict['soilmoisture2']),
+        'soilatt2' : float(convertedDict['soilatt2']),
+        'soilmoisture3' : float(convertedDict['soilmoisture3']),
+        'soilatt3' : float(convertedDict['soilatt3'])
     }
 
-    sensorDict2 = {
-        'doctype' : 'sensor',
-        'sensorid' : '2',
-        'eventtime' : str(convertedDict['dateutc']),
-        'processingtime' : timestr,
-        'soilmoisture' : float(convertedDict['soilmoisture2']),
-        'soilatt' : float(convertedDict['soilatt2']),
-    }
-
-    sensorDict3 = {
-        'doctype' : 'sensor',
-        'sensorid' : '3',
-        'eventtime' : str(convertedDict['dateutc']),
-        'processingtime' : timestr,
-        'soilmoisture' : float(convertedDict['soilmoisture3']),
-        'soilatt' : float(convertedDict['soilatt3']),
-    }
-
-    stationDict = {
-        'doctype' : 'station',
+    outputDict = {
         'stationtype' : str(convertedDict['stationtype']),
-        'eventtime' : str(convertedDict['dateutc']),
-        'processingtime' : timestr,
+        'sensordateutc' : str(convertedDict['dateutc']),
+        'functiondatelocal' : str(datetime.now(pytz.timezone('US/Eastern'))),
         'tempinf' : float(convertedDict['tempinf']),
         'humidityin' : float(convertedDict['humidityin']),
         'aromrelin' : float(convertedDict['aromrelin']),
         'aromasin' : float(convertedDict['aromasin']),
         'freq' : str(convertedDict['freq']),
-        'model' : str(convertedDict['model'])
+        'model' : str(convertedDict['model']),
+        'soilsensors' : sensorDict
     }
    
-    return stationDict, sensorDict1, sensorDict2, sensorDict3
+    return outputDict
+
+def str_map_ref(str1):
+
+    str1 = '{"' + str1\
+    .replace("b","")\
+    .replace('"', '')\
+    .replace("'", '')\
+    .replace(",", '","')\
+    .replace("=", '":"')\
+    .replace("&", '","')\
+    .replace("PASSKEY:", 'PASSKEY":"') + '"}'
+
+    convertedDict = json.loads(str1)
+
+    sensorDict = {
+        'soilmoisture1' : float(convertedDict['soilmoisture1']),
+        'soilatt1' : float(convertedDict['soilatt1']),
+        'soilmoisture2' : float(convertedDict['soilmoisture2']),
+        'soilatt2' : float(convertedDict['soilatt2']),
+        'soilmoisture3' : float(convertedDict['soilmoisture3']),
+        'soilatt3' : float(convertedDict['soilatt3'])
+    }
+
+    outputDict = {
+        'stationtype' : str(convertedDict['stationtype']),
+        'sensordateutc' : str(convertedDict['dateutc']),
+        'functiondatelocal' : str(datetime.now(pytz.timezone('US/Eastern'))),
+        'tempinf' : float(convertedDict['tempinf']),
+        'humidityin' : float(convertedDict['humidityin']),
+        'aromrelin' : float(convertedDict['aromrelin']),
+        'aromasin' : float(convertedDict['aromasin']),
+        'freq' : str(convertedDict['freq']),
+        'model' : str(convertedDict['model']),
+        'soilsensors' : sensorDict
+    }
+   
+    return outputDict
--- httpingest/__init__.py
+++ httpingest/__init__.py
@@ -2,25 +2,46 @@ import logging
 import json
 import azure.functions as func
 from .functions.EcoWittPrep import *
+import pytz
+from datetime import datetime
 
-def main(req: func.HttpRequest, doc: func.Out[func.Document]) -> func.HttpResponse:
+def main(req: func.HttpRequest, ex_emb: func.Out[func.Document], ex_ref: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     input_str = str(req.get_body())
-    logging.info('http body: ' + input_str)
+    logging.info('http body: ' + input_str) # log message recieved
 
-    output_dict =  str_map(input_str)
+    processingtime = str(datetime.now(pytz.timezone('US/Eastern'))) # declare processing time
+
+    output_lst_emb = [] # list of output something
+    output_lst_ref = [] # list of output something
+
+    station_doc = str_map_emb(input_str, processingtime) # calculate embedded docs
+    station_doc_ref, sensor_doc1, sensor_doc2, sensor_doc3 = str_map_ref(input_str, processingtime)
+
+    output_lst_emb.append(station_doc)
+
+    output_lst_ref.append(station_doc_ref)
+    output_lst_ref.append(sensor_doc1)
+    output_lst_ref.append(sensor_doc2)
+    output_lst_ref.append(sensor_doc3)
 
     ## Logging only
-    convertedString = json.dumps(output_dict, default=str)
-    logging.info('json string: ' + convertedString)
+    # convertedString = json.dumps(output_dict, default=str)
+    # logging.info('json string: ' + convertedString)
+    ##
 
-    newdocs = func.DocumentList() 
+    newdocs_emb = ex_emb.DocumentList()
 
-    newdocs.append(func.Document.from_dict(output_dict))
+    for item in output_lst_emb:
+        newdocs_emb.append(func.Document.from_dict(item))
+    ex_emb.set(newdocs_emb)
 
-    print(newdocs.data)
 
-    doc.set(newdocs)
+    newdocs_ref = ex_ref.DocumentList()
+
+    for item in output_lst_ref:
+        newdocs_ref.append(func.Document.from_dict(item))
+    ex_ref.set(newdocs_ref)
 
     return 'success'
