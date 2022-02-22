5c2bf48c1d40a23828b244900930edc2b04e4e16
Chas Fiorenza <chasfiorenza@gmail.com>
2022-02-22 15:46:15 -0500

    change to sdk write

--- httpingest/__init__.py
+++ httpingest/__init__.py
@@ -3,45 +3,57 @@ import azure.functions as func
 from .functions.EcoWittPrep import *
 import pytz
 from datetime import datetime
+import os
 
-def main(req: func.HttpRequest, ex_emb: func.Out[func.Document], ex_ref: func.Out[func.Document]) -> func.HttpResponse:
+   
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+from azure.cosmos.partition_key import PartitionKey
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
     # , ex_emb: func.Out[func.Document], ex_ref: func.Out[func.Document]
     logging.info('Python HTTP trigger function processed a request.')
 
     input_str = str(req.get_body())
-    # logging.info('http body: ' + input_str) # log message recieved
 
-    # processingtime = str(datetime.now(pytz.timezone('US/Eastern'))) # declare processing time
+    def create_items(container,item):
+        client = cosmos_client(os.environ['cosmosEndPoint'], os.environ['cosmosKey'])
 
-    # output_lst_emb = [] # list of output something
-    # output_lst_ref = [] # list of output something
+        database = client.get_database_client('telemetry')
 
-    # station_doc = str_map_emb(input_str, processingtime) # calculate embedded docs
-    # station_doc_ref, sensor_doc1, sensor_doc2, sensor_doc3 = str_map_ref(input_str, processingtime)
+        # container = database.get_container_client('climate_ref')
+        container = database.get_container_client('climate_ref')
 
-    # output_lst_emb.append(station_doc)
+        container.create_item(body=item)
 
-    # output_lst_ref.append(station_doc_ref)
-    # output_lst_ref.append(sensor_doc1)
-    # output_lst_ref.append(sensor_doc2)
-    # output_lst_ref.append(sensor_doc3)
+    logging.info('http body: ' + input_str) # log message recieved
 
-    # ## Logging only
-    # # convertedString = json.dumps(output_dict, default=str)
-    # # logging.info('json string: ' + convertedString)
-    # ##
+    processingtime = str(datetime.now(pytz.timezone('US/Eastern'))) # declare processing time
 
-    # newdocs_emb = ex_emb.DocumentList()
+    output_lst_emb = [] # list of output something
+    output_lst_ref = [] # list of output something
+
+    station_doc = str_map_emb(input_str, processingtime) # calculate embedded docs
+    station_doc_ref, sensor_doc1, sensor_doc2, sensor_doc3 = str_map_ref(input_str, processingtime)
+
+    output_lst_emb.append(station_doc)
+
+    output_lst_ref.append(station_doc_ref)
+    output_lst_ref.append(sensor_doc1)
+    output_lst_ref.append(sensor_doc2)
+    output_lst_ref.append(sensor_doc3)
+
+    ## Logging only
+    # convertedString = json.dumps(output_dict, default=str)
+    # logging.info('json string: ' + convertedString)
+    ##
 
     # for item in output_lst_emb:
-    #     newdocs_emb.append(func.Document.from_dict(item))
-    # ex_emb.set(newdocs_emb)
+        # newdocs_emb.append(func.Document.from_dict(item))
 
 
-    # newdocs_ref = ex_ref.DocumentList()
+    for item in output_lst_ref:
+        create_items('climate_ref',item)
 
-    # for item in output_lst_ref:
-    #     newdocs_ref.append(func.Document.from_dict(item))
-    # ex_ref.set(newdocs_ref)
 
     return 'success'
