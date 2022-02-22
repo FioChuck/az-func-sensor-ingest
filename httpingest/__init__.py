8808fffb691237b972cd850503ac9fb2892be391
Chas Fiorenza <chasfiorenza@gmail.com>
2022-02-22 15:09:35 -0500

    change destination function

--- httpingest/__init__.py
+++ httpingest/__init__.py
@@ -4,43 +4,44 @@ from .functions.EcoWittPrep import *
 import pytz
 from datetime import datetime
 
-def main(req: func.HttpRequest, ex_emb: func.Out[func.Document], ex_ref: func.Out[func.Document]) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    # , ex_emb: func.Out[func.Document], ex_ref: func.Out[func.Document]
     logging.info('Python HTTP trigger function processed a request.')
 
     input_str = str(req.get_body())
-    logging.info('http body: ' + input_str) # log message recieved
+    # logging.info('http body: ' + input_str) # log message recieved
 
-    processingtime = str(datetime.now(pytz.timezone('US/Eastern'))) # declare processing time
+    # processingtime = str(datetime.now(pytz.timezone('US/Eastern'))) # declare processing time
 
-    output_lst_emb = [] # list of output something
-    output_lst_ref = [] # list of output something
+    # output_lst_emb = [] # list of output something
+    # output_lst_ref = [] # list of output something
 
-    station_doc = str_map_emb(input_str, processingtime) # calculate embedded docs
-    station_doc_ref, sensor_doc1, sensor_doc2, sensor_doc3 = str_map_ref(input_str, processingtime)
+    # station_doc = str_map_emb(input_str, processingtime) # calculate embedded docs
+    # station_doc_ref, sensor_doc1, sensor_doc2, sensor_doc3 = str_map_ref(input_str, processingtime)
 
-    output_lst_emb.append(station_doc)
+    # output_lst_emb.append(station_doc)
 
-    output_lst_ref.append(station_doc_ref)
-    output_lst_ref.append(sensor_doc1)
-    output_lst_ref.append(sensor_doc2)
-    output_lst_ref.append(sensor_doc3)
+    # output_lst_ref.append(station_doc_ref)
+    # output_lst_ref.append(sensor_doc1)
+    # output_lst_ref.append(sensor_doc2)
+    # output_lst_ref.append(sensor_doc3)
 
-    ## Logging only
-    # convertedString = json.dumps(output_dict, default=str)
-    # logging.info('json string: ' + convertedString)
-    ##
+    # ## Logging only
+    # # convertedString = json.dumps(output_dict, default=str)
+    # # logging.info('json string: ' + convertedString)
+    # ##
 
-    newdocs_emb = ex_emb.DocumentList()
+    # newdocs_emb = ex_emb.DocumentList()
 
-    for item in output_lst_emb:
-        newdocs_emb.append(func.Document.from_dict(item))
-    ex_emb.set(newdocs_emb)
+    # for item in output_lst_emb:
+    #     newdocs_emb.append(func.Document.from_dict(item))
+    # ex_emb.set(newdocs_emb)
 
 
-    newdocs_ref = ex_ref.DocumentList()
+    # newdocs_ref = ex_ref.DocumentList()
 
-    for item in output_lst_ref:
-        newdocs_ref.append(func.Document.from_dict(item))
-    ex_ref.set(newdocs_ref)
+    # for item in output_lst_ref:
+    #     newdocs_ref.append(func.Document.from_dict(item))
+    # ex_ref.set(newdocs_ref)
 
     return 'success'
