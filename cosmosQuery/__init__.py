860aaf9800f473f5a6be26e3ee6091efb08884d5
Chas Fiorenza <chasfiorenza@gmail.com>
2022-02-24 10:18:25 -0500

    increase query 100x

--- cosmosQuery/__init__.py
+++ cosmosQuery/__init__.py
@@ -6,6 +6,9 @@ import json
 import azure.functions as func
 import pandas as pd
 
+logger = logging.getLogger("azure.core.pipeline.policies.http_logging_policy")
+logger.setLevel(logging.WARNING)
+
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
@@ -20,11 +23,13 @@ def main(mytimer: func.TimerRequest) -> None:
     database = client.get_database_client('telemetry')
     container = database.get_container_client('openweathermap-description')
 
-    out = container.query_items(query = 'SELECT * FROM c WHERE c.weather.description = "clear sky"',populate_query_metrics = True, enable_cross_partition_query = True)
+    for x in range(100):
+        out = container.query_items(query = 'SELECT * FROM c WHERE c.weather.description = "clear sky"',populate_query_metrics = True, enable_cross_partition_query = True)
 
-    for item in out:
-        dflist.append(dict(item))
-        
-    df = pd.DataFrame(dflist)
-
-    logging.info('Query Result Count: ' + str(df.shape) )
+        for item in out:
+            dflist.append(dict(item))
+            
+        df = pd.DataFrame(dflist)
+        logging.info('Query Number' + str(x) )
+        logging.info('Query Result Count: ' + str(df.shape) )
+    
