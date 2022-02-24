9b809f88ad635b86563c473e696029acfda88afe
Chas Fiorenza <chasfiorenza@gmail.com>
2022-02-24 11:35:13 -0500

    duplicated functions

--- cosmosQuery1/__init__.py
+++ cosmosQuery1/__init__.py
@@ -21,13 +21,13 @@ def main(mytimer: func.TimerRequest) -> None:
     database = client.get_database_client('telemetry')
     container = database.get_container_client('openweathermap-description')
 
-    for x in range(10):
-        out = container.query_items(query = 'SELECT * FROM c WHERE c.weather.description = "overcast clouds"',populate_query_metrics = True, enable_cross_partition_query = True)
-        dflist = []
-        for item in out:
-            dflist.append(dict(item))
-            
-        df = pd.DataFrame(dflist)
-        logging.info('Query Number' + str(x) )
-        logging.info('Query Result Count: ' + str(df.shape) )
+    # for x in range(10):
+    out = container.query_items(query = 'SELECT * FROM c WHERE c.weather.description = "overcast clouds"',populate_query_metrics = True, enable_cross_partition_query = True)
+    dflist = []
+    for item in out:
+        dflist.append(dict(item))
+        
+    df = pd.DataFrame(dflist)
+    # logging.info('Query Number' + str(x) )
+    logging.info('Query Result Count: ' + str(df.shape) )
     
